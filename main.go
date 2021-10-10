66b3fb0fee962caad2c51f24a52399b757c4e778
LAPTOP-9ND4HRG6\xhy <383164014@qq.com>
2021-10-10 20:14:11 +0800

    标准HTTP框架

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	// 初始化配置
 	configs.Init(configs.WithRedis(), configs.WithMySQL())
 	// 初始化 mysql 、redis 等服务组件
-	ComponentInit(RegisterMySQL(), RegisterMySQL(), RegisterLogger())
+	ComponentInit(RegisterRedis(), RegisterMySQL(), RegisterLogger())
 	// 中间件
 	g.Use(middleware.Cors())
 	// 初始化路由
